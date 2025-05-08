8f78ed84fea7ea291fad5fe182e78b6249640baa
Chamir <samir.castillo.r@gmail.com>
2025-05-08 15:46:59 -0500

    first commit

---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import {  Calendar, PackageOpen, History  } from 'lucide-vue-next';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Prestamo',
+        href: '/dashboard',
+    },
+];
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex items-center pl-4">
+                    <PackageOpen class="w-50 h-50 " stroke-width="1" />
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex items-center justify-center">
+                    <Calendar class="w-50 h-50 " stroke-width="1" />
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex items-center justify-center">
+                    <History class="w-50 h-50 " stroke-width="1" />
+                </div>
+            </div>
+            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
+
+            </div>
+        </div>
+    </AppLayout>
+</template>
