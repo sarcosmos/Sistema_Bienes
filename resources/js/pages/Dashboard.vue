2b7892a30796fc3e4390fc3286e1dc2c413a8c40
Chamir <samir.castillo.r@gmail.com>
2025-05-14 10:39:13 -0500

    Pagina principal

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -3,7 +3,7 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
-import {  Calendar, PackageOpen, History  } from 'lucide-vue-next';
+import {  PackageOpen,ChartNoAxesColumn,FileSpreadsheet,Archive,PackageCheck,FileCheck,User  } from 'lucide-vue-next';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -18,19 +18,35 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex items-center pl-4">
-                    <PackageOpen class="w-50 h-50 " stroke-width="1" />
+            <div class="grid auto-rows-min gap-5 md:grid-cols-4">
+                <div class="relative aspect-video overflow-hidden rounded-xl flex items-center pl-4 dark:bg-red-500 bg-red-400 ">
+                    <PackageOpen class="w-25 h-30 " stroke-width="0.8" />
+                    <span class="text-2xl font-medium pl-12 mb-35">Prestamos</span>
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex items-center justify-center">
-                    <Calendar class="w-50 h-50 " stroke-width="1" />
+                <div class="relative aspect-video overflow-hidden rounded-xl flex items-center pl-4 dark:bg-blue-500 bg-blue-400 ">
+                    <ChartNoAxesColumn class="w-25 h-30 " stroke-width="1" />
+                    <span class="text-2xl font-medium pl-4 mb-35">Inventario Actual</span>
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex items-center justify-center">
-                    <History class="w-50 h-50 " stroke-width="1" />
+                <div class="relative aspect-video overflow-hidden rounded-xl flex items-center pl-4 dark:bg-green-500 bg-green-400">
+                    <FileSpreadsheet class="w-25 h-30 " stroke-width="1" />
+                    <span class="text-2xl font-medium pl-12 mb-35">Facturas de Prestamos</span>
                 </div>
-            </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
-
+                <div class="relative aspect-video overflow-hidden rounded-xl flex items-center pl-4 dark:bg-yellow-500 bg-yellow-400">
+                    <Archive class="w-25 h-30 " stroke-width="1" />
+                    <span class="text-2xl font-medium pl-18 mb-35">Bienes</span>
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl flex items-center pl-4 dark:bg-purple-500 bg-purple-400">
+                    <PackageCheck class="w-25 h-30 " stroke-width="1" />
+                    <span class="text-2xl font-medium pl-12 mb-35">Devoluciones</span>
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl flex items-center pl-4 dark:bg-pink-500 bg-pink-400">
+                    <FileCheck class="w-25 h-30 " stroke-width="1" />
+                    <span class="text-2xl font-medium pl-12 mb-35">Facturas de Devoluciones</span>
+                </div>
+                <div class="relative aspect-video overflow-hidden rounded-xl flex items-center pl-4 dark:bg-teal-500 bg-teal-400">
+                    <User class="w-25 h-30 " stroke-width="1" />
+                    <span class="text-2xl font-medium pl-12 mb-35">Usuarios</span>
+                </div>
             </div>
         </div>
     </AppLayout>
